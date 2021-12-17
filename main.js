e6a6620da29483cd4ff9c7b448ba6fdd0421047f
Ullua <juanulluazz@gmail.com>
2021-12-17 14:04:24 -0300

    ultima actualizacion

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 gsap.registerPlugin(ScrollTrigger);
 const tl = gsap.timeline({
     repeat: 0,
-})
+});
+const tl2 = gsap.timeline({
+  repeat: 0,
+});
 
 // let tween = gsap.from(".scrollPrueba", {x:-50, duration: 1, opacity: 0,}),
 //     st = ScrollTrigger.create({
@@ -161,3 +164,76 @@ gsap.from('.scrollPrueba', {
             stagger: .6,
     
         },'-=1' );
+
+
+
+
+
+        // tl2.from('.segunda-parte', {
+        //   scrollTrigger: {
+        //     trigger: "#ventajas-parrafo",
+        //     start: "top center",
+        //     end: ()=> '+=',
+        //    markers: true,
+        //   },
+        //   y: '50%',
+        //   opacity: 0,
+        //   duration: 2,
+        //   ease: Power4.easeOut,
+        // }
+        // );
+
+
+
+//         tl2.from('.fila-animada', {
+//           scrollTrigger: {
+//             trigger: '#ventajas-parrafo',
+//             start: "top center",
+//             end: ()=> '+=',
+//             // markers: true,
+//           },
+//           y: 100,
+//           opacity: 0,
+//           duration: 1,
+//           stagger: .6,
+  
+//       }, '-=1');    
+//       tl2.from('.fila-animada2', {
+//         scrollTrigger: {
+//           trigger: '.fila-animada',
+//           start: "top center",
+//           end: ()=> '+=',
+//           // markers: true,
+//         },
+//         y: 100,
+//         opacity: 0,
+//         duration: 1,
+//         stagger: .6,
+
+//     },'-=1' );    
+//     tl2.from('.fila-animada3', {
+//       scrollTrigger: {
+//         trigger: '.fila-animada3',
+//         start: "top center",
+//         end: ()=> '+=',
+//         // markers: true,
+//       },
+//       y: 100,
+//       opacity: 0,
+//       duration: 1,
+//       stagger: .6,
+
+//   },'-=1' );    
+//   tl2.from('.fila-animada4', {
+//     scrollTrigger: {
+//       trigger: '.fila-animada',
+//       start: "top center",
+//       end: ()=> '+=',
+//       // markers: true,
+//     },
+//     y: 100,
+//     opacity: 0,
+//     duration: 1,
+//     stagger: .6,
+
+// }, '-=1');    
